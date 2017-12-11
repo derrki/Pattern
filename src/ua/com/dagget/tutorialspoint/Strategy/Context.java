0adbeef5846a5cbc487673acb0964a4c0883d183
Roman_Kaminskiy <derrki@gmail.com>
2017-12-11 23:35:43 +0200

    Додано опис патерну.

--- src/ua/com/dagget/tutorialspoint/Strategy/Context.java
+++ src/ua/com/dagget/tutorialspoint/Strategy/Context.java
@@ -1,5 +1,12 @@
 package ua.com.dagget.tutorialspoint.Strategy;
 
+/**
+ * Strategy. Intent. Define a family of algorithms, encapsulate each one,
+ * and make them interchangeable. Strategy lets the algorithm vary independently from clients that use it.
+ *Стратегія. Призначення. Визначає сім’ю алгоритмів, інкапсулює кожен з них і робить їх взаємозамінними.
+ * Стратегія дозволяє видозмінювати алгоритм незалежно від клієнтського коду, що використовує стратегію.
+ */
+
 public class Context {
     private Strategy strategy;
 
